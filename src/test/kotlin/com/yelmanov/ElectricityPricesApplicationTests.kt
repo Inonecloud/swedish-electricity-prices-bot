baf5457420a682ed282b4e6b823b2c04348d5540
andrey <andrewyelmanov@gmail.com>
2023-09-28 20:37:17 +0200

    Added dockerfile

--- src/test/kotlin/com/yelmanov/ElectricityPricesApplicationTests.kt
+++ src/test/kotlin/com/yelmanov/ElectricityPricesApplicationTests.kt
@@ -1,12 +1,9 @@
 package com.yelmanov
 
-import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
 class ElectricityPricesApplicationTests {
 
-    @Test
+
     fun contextLoads() {
     }
 
